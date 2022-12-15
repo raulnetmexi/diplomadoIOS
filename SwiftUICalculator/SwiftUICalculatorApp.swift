9957451eabde2351043f82615ecdb322bb74d0f6
raulnetmexican <raul.oropeza.cussi@gmail.com>
2022-12-14 23:16:26 -0600

    App Calculadora IOS

---
+++ SwiftUICalculator/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  SwiftUICalculator
+//
+//  Created by Raul Oropeza on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
